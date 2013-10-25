883a1e649081f7b540006806f1254ff013d9479e
every2ndcountsxc <jamesbarracca@yahoo.com>
2013-10-25 16:03:36 -0700

    small tweak

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -45,7 +45,7 @@ require(['app', 'jquery', 'bootstrap', 'stellar', 'tooltip'], function (app, $) 
 
         var top = $(document).scrollTop();
 
-        if (top > 200) {
+        if (top > 220) {
             $('.top-bg').addClass('hide');
 
         }
